c6df60c7c88849102d15d8a3dd775a9fdb1720ed
Ivan <ivan.pgs.3@gmail.com>
2018-05-08 16:29:17 +0300

    SecondCommit

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -11,11 +11,13 @@ namespace ConsoleTest
         {
             CarManager manager = new CarManager();
             manager.GetCarsFromFile();
-            Car eCar = new Car();
-            eCar = manager.FindCarByID("73dab7a0-7f8b-432b-b494-cb365ed92953");
-            Console.WriteLine(eCar);
-            //eCar.Drive(3, 50);
-            //manager.UpdateCarInFile(eCar);
+            Car eCar = manager.FindCarByID("73dab7a0-7f8b-432b-b494-cb365ed92953");
+            Console.WriteLine(eCar.Milage);
+            eCar.StartEngine();
+            eCar.Drive(3, 50);
+            eCar.StopEngine();
+            manager.UpdateCarInFile(eCar);
+            Console.WriteLine(eCar.Milage);
 
             Console.ReadKey();
         }
