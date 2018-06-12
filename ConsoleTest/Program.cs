e25cc128d1763efb711851cb4e6fa34b9776cc14
Ivan <ivan.pgs.3@gmail.com>
2018-06-12 19:29:28 +0300

    DelegateExamples #1

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -6,24 +6,57 @@ using PracticeProject.Core.Model;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using static DelegateExamples.Calculator;
 
 namespace ConsoleTest
 {
 
     class Program
     {
+        //методы для делегата
+        private static double Add(double x, double y)
+        {   
+                    return x+y;
+
+        }
+
+        private static double Diff(double x, double y)
+        {
+            return x - y;
+        }
+
+        private static double Div(double x, double y)
+        {
+            //return x != 0 ? x/y :0;
+            return x / y;
+        }
+
+        private static double Multiply(double x, double y)
+        {
+            return x *y;
+        }
+
         static void Main(string[] args)
         {
 
-            CarXmlManager xmlCarManager = new CarXmlManager();
+
+            Operation add = new Operation(Add);
+            double resultAdd = add.Invoke(2, 2);
+            Console.WriteLine(resultAdd);
+
+            Operation div = new Operation(Div);
+            double resultDiv = div.Invoke(2, 2);
+            Console.WriteLine(resultDiv);
+
+            //CarXmlManager xmlCarManager = new CarXmlManager();
             //Console.WriteLine(xmlCarManager.GetCars().Count);
-            Car car = xmlCarManager.FindCarById("abd4deaf-3212-4d98-bc8f-e1ec1e9443fa");
-            car.Name = "X7";
-            xmlCarManager.UpdateCar(car);
-            ////Console.WriteLine(car.ConsoleView());
+            //Car car = xmlCarManager.FindCarById("abd4deaf-3212-4d98-bc8f-e1ec1e9443fa");
+            //car.Name = "X7";
+            //xmlCarManager.UpdateCar(car);
+            //Console.WriteLine(car.ConsoleView());
             //xmlCarManager.DeleteCar(car);
             //Console.WriteLine(xmlCarManager.GetCars().Count);
-            ////Console.WriteLine(car.ConsoleView());
+            //Console.WriteLine(car.ConsoleView());
 
 
             //Car car = xmlCarManager.FindCarById("a8bc7152-9ae5-4c4a-8fe8-f9e30f06647e");
@@ -40,7 +73,7 @@ namespace ConsoleTest
             //    Power = 112,
             //    MaxSpeed = 220,
             //    Milage = 10000,
-            //    YearOfProduction = new DateTime(2010,03, 20),
+            //    YearOfProduction = new DateTime(2010, 03, 20),
             //    isAvailable = true
             //};
 
---
+++ DelegateExamples/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateExamples
+{
+    public class Calculator
+    {
+        public delegate double Operation(double x,double y); // делегат
+
+    }
+}
